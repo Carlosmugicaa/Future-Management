9a8ed4110488dd469a326d129d19f3e471d0574c
Carlos Mugica <cmugica4@cloud.com>
2022-02-11 13:03:54 -0700

    Initial Commit

---
+++ Future Management/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Future Management
+//
+//  Created by Carlos Mugica on 2/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
